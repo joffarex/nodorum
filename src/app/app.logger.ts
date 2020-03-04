7062461ce477f2ebe27ca8bb5bb1bde19a73cf0b
Tornike Goshadze <joffarex@gmail.com>
2020-03-04 21:42:43 +0400

    logger(): add label and formatting to logger

--- src/app/app.logger.ts
+++ src/app/app.logger.ts
@@ -1,16 +1,22 @@
 import { LoggerService } from '@nestjs/common';
-import { transports, createLogger, Logger } from 'winston';
+import { transports, createLogger, Logger, format } from 'winston';
 import { config } from '../config';
+
+const myFormat = format.printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
 
 export class AppLogger implements LoggerService {
 	private logger: Logger;
 
-	constructor() {
+	constructor(label: string) {
 		this.logger = createLogger({
 			level: config.logger.level,
+			format: format.combine(format.label({label}), format.timestamp(), myFormat),
 			transports: [
         new transports.File({ filename: 'quick-start-error.log', level: 'error' }),
-        new transports.File({ filename: 'quick-start-combined.log' })
+				new transports.File({ filename: 'quick-start-combined.log' }),
+				new transports.Console()
 			]
 		});
 	}
