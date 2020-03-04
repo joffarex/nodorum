a519559ca7f71aa638dfe9dc5ec101756b6433f3
Tornike Goshadze <joffarex@gmail.com>
2020-03-05 00:36:15 +0400

    auth(): add auth guard on users

---
+++ src/app/shared/guards/auth.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { FastifyRequest } from 'fastify';
+import { AuthService } from 'src/app/auth/auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly authService: AuthService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<FastifyRequest>();
+    const authorization: string = request.headers.authorization;
+    if (!authorization) {
+      return false;
+    }
+
+    const token = authorization.split(' ')[1];
+    if (!token) {
+      return false;
+    }
+
+    const user = await this.authService.verifyJwtToken(token);
+
+    if (!user) {
+      return false;
+    }
+
+    request.user = user;
+
+    return true;
+  }
+}
--- src/app/user/user.controller.ts
+++ src/app/user/user.controller.ts
@@ -1,43 +1,42 @@
-import { Controller, Get, Param, Post, Body, Put, Delete, UsePipes } from '@nestjs/common';
-import {LogoutUserDto, UpdateUserDto } from './dto';
+import { Controller, Get, Param, Body, Put, Delete, UsePipes, UseGuards, Scope, Inject } from '@nestjs/common';
+import { UpdateUserDto } from './dto';
 import { UserService } from './user.service';
 import { UserBody } from './interfaces/user.interface';
 import { updateSchema } from './validator';
 import { JoiValidationPipe } from '../shared/pipes/joi-validation.pipe';
+import { AuthGuard } from '../shared/guards/auth.guard';
+import { REQUEST } from '@nestjs/core';
+import { FastifyRequest } from 'fastify';
 
-@Controller('users')
+@Controller({
+  path: 'users',
+  scope: Scope.REQUEST,
+})
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(@Inject(REQUEST) private readonly request: FastifyRequest, private readonly userService: UserService) {}
 
   @Get('/me')
+  @UseGuards(AuthGuard)
   async me(): Promise<UserBody> {
-    // will get user id from token, currently hard-coded
-    return this.userService.findOne(1);
+    return this.userService.findOne(this.request.user.id);
   }
 
   @Get('/:id')
   // @Roles('user')
   async findOne(@Param('id') id: number): Promise<UserBody> {
-    return this.userService.findOne(id); // possibly decode from base64
-  }
-
-  // only implemented after auth has been implemented
-  @Post('/logout')
-  async logout(@Body() logoutUserDto: LogoutUserDto): Promise<LogoutUserDto> {
-    return logoutUserDto;
+    return this.userService.findOne(id);
   }
 
   @Put('/update')
+  @UseGuards(AuthGuard)
   @UsePipes(new JoiValidationPipe(updateSchema))
   async update(@Body() updateUserDto: UpdateUserDto): Promise<UserBody> {
-    // id is currently hard-coded
-    // that is because authentication system is not built yet.
-    // after that, id will be fetched from token
-    return this.userService.update(1, updateUserDto);
+    return this.userService.update(this.request.user.id, updateUserDto);
   }
 
   @Delete('/delete')
+  @UseGuards(AuthGuard)
   async delete(): Promise<{ message: string }> {
-    return this.userService.delete(1);
+    return this.userService.delete(this.request.user.id);
   }
 }
