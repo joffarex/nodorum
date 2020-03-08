0a67d047f90f72cba9547b20ca73f06356218da8
Tornike Goshadze <joffarex@gmail.com>
2020-03-08 18:26:01 +0400

    chore(): remove redundant function

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,15 +1,5 @@
-import crypto from 'crypto';
-import { config } from 'src/config';
-
 export * from './entity';
 export * from './filters';
 export * from './request-context';
 export * from './middleware/request-context.middleware';
 export * from './deep-partial';
-
-export function passwordHash(password: string) {
-  return crypto
-    .createHmac('sha256', config.salt)
-    .update(password, 'utf8')
-    .digest('hex');
-}
