b836ede28bdc42e2a26b323bc6adaf1ee6dd13cc
Tornike Goshadze <joffarex@gmail.com>
2020-03-10 23:49:36 +0400

    logformat(): add log formatter

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -3,3 +3,4 @@ export * from './filters';
 export * from './request-context';
 export * from './middleware/request-context.middleware';
 export * from './deep-partial';
+export * from './log-format';
