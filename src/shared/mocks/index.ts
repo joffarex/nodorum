83803eeb21a22cfe2adda1d18d09b09524ca1e82
Tornike Goshadze <joffarex@gmail.com>
2020-03-16 00:13:01 +0400

    mock(): expand repository mock

--- src/shared/mocks/index.ts
+++ src/shared/mocks/index.ts
@@ -2,7 +2,13 @@ export const selectSpy = jest.fn().mockReturnThis();
 export const leftJoinAndSelectSpy = jest.fn().mockReturnThis();
 export const whereSpy = jest.fn().mockReturnThis();
 export const getOneSpy = jest.fn();
+export const findOneSpy = jest.fn();
 export const getRawOneSpy = jest.fn();
+export const getCountSpy = jest.fn();
+export const orderBySpy = jest.fn().mockReturnThis();
+export const limitSpy = jest.fn().mockReturnThis();
+export const offsetSpy = jest.fn().mockReturnThis();
+export const getManySpy = jest.fn();
 
 export const mockRepositoryFactory = jest.fn(() => ({
   createQueryBuilder: jest.fn(() => ({
@@ -11,5 +17,11 @@ export const mockRepositoryFactory = jest.fn(() => ({
     where: whereSpy,
     getOne: getOneSpy,
     getRawOne: getRawOneSpy,
+    getCount: getCountSpy,
+    getMany: getManySpy,
+    orderBy: orderBySpy,
+    limit: limitSpy,
+    offset: offsetSpy,
   })),
+  findOne: findOneSpy,
 }));
