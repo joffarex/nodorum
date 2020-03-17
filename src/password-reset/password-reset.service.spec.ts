e4a460a99852e2c48260275ee1aa28de92f9fc78
tornike Goshadze <joffarex@gmail.com>
2020-03-17 22:47:49 +0400

    test(reset): add all password reset tests

--- src/password-reset/password-reset.service.spec.ts
+++ src/password-reset/password-reset.service.spec.ts
@@ -1,18 +1,127 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Provider, NotFoundException, BadRequestException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MailerModule } from '@nest-modules/mailer';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import stubTransport from 'nodemailer-stub-transport';
 import { PasswordResetService } from './password-reset.service';
+import { PasswordResetEntity } from './password-reset.entity';
+import { UserEntity } from '../user/user.entity';
+import { getOneSpy, mockRepositoryFactory, findOneSpy, saveSpy, deleteSpy } from '../shared/mocks/spies.mock';
+import {
+  mockUserOne,
+  mockPasswordResetOne,
+  mockQueryOne,
+  mockQueryTwo,
+  mockPasswordResetTwo,
+} from '../shared/mocks/data.mock';
 
 describe('PasswordResetService', () => {
-  let service: PasswordResetService;
+  let passwordResetService: PasswordResetService;
+
+  const mockRepositories: Provider[] = [];
+  const repositoryTokenEntities = [PasswordResetEntity, UserEntity];
+
+  for (const entity of repositoryTokenEntities) {
+    mockRepositories.push({
+      provide: getRepositoryToken(entity),
+      useFactory: mockRepositoryFactory,
+    });
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PasswordResetService],
+      imports: [
+        MailerModule.forRootAsync({
+          useFactory: () => ({
+            transport: stubTransport(),
+            defaults: {
+              from: '"noddit" <no-reply@noddit.app>',
+            },
+          }),
+        }),
+      ],
+      providers: [PasswordResetService, ConfigService, ...mockRepositories],
     }).compile();
 
-    service = module.get<PasswordResetService>(PasswordResetService);
+    passwordResetService = module.get<PasswordResetService>(PasswordResetService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(passwordResetService).toBeDefined();
+  });
+
+  it('should send forgot password email', async () => {
+    findOneSpy.mockReturnValueOnce(mockUserOne);
+    saveSpy.mockReturnValueOnce(mockPasswordResetOne);
+
+    const res = await passwordResetService.forgotPassword({ email: mockUserOne.email });
+
+    expect(res).toStrictEqual({ message: `Email sent to ${mockUserOne.email}` });
+  });
+
+  it('should throw user not found exception in forgot password', async () => {
+    findOneSpy.mockReturnValue(undefined);
+
+    await expect(passwordResetService.forgotPassword({ email: 'test@test.com' })).rejects.toBeInstanceOf(
+      NotFoundException,
+    );
+    await expect(passwordResetService.forgotPassword({ email: 'test@test.com' })).rejects.toThrowError(
+      'User not found',
+    );
+  });
+
+  it('should reset password', async () => {
+    const body = {
+      password: 'updatedpass',
+    };
+
+    findOneSpy.mockReturnValueOnce(mockPasswordResetOne);
+    getOneSpy.mockReturnValueOnce(mockUserOne);
+    deleteSpy.mockReturnValueOnce({});
+
+    const res = await passwordResetService.resetPassword(mockQueryOne, body);
+
+    expect(res).toStrictEqual({ message: 'Password reset successfully' });
+  });
+
+  it('should throw bad request exception when reset does not exist', async () => {
+    const body = {
+      password: 'updatedpass',
+    };
+
+    findOneSpy.mockReturnValue(undefined);
+
+    await expect(passwordResetService.resetPassword(mockQueryOne, body)).rejects.toBeInstanceOf(BadRequestException);
+    await expect(passwordResetService.resetPassword(mockQueryOne, body)).rejects.toThrowError(
+      'Invalid password reset token',
+    );
+  });
+
+  it('should throw bad request exception when reset is not valid', async () => {
+    const body = {
+      password: 'updatedpass',
+    };
+
+    findOneSpy.mockReturnValue(mockPasswordResetTwo);
+    await expect(passwordResetService.resetPassword(mockQueryOne, body)).rejects.toBeInstanceOf(BadRequestException);
+    await expect(passwordResetService.resetPassword(mockQueryOne, body)).rejects.toThrowError(
+      'Invalid password reset token',
+    );
+
+    findOneSpy.mockReturnValue(mockPasswordResetOne);
+
+    await expect(passwordResetService.resetPassword(mockQueryTwo, body)).rejects.toBeInstanceOf(BadRequestException);
+    await expect(passwordResetService.resetPassword(mockQueryTwo, body)).rejects.toThrowError(
+      'Invalid password reset token',
+    );
+
+    findOneSpy.mockReturnValueOnce(mockPasswordResetOne);
+    getOneSpy.mockReturnValueOnce(undefined);
+    await expect(passwordResetService.resetPassword(mockQueryOne, body)).rejects.toBeInstanceOf(BadRequestException);
+
+    await expect(passwordResetService.resetPassword(mockQueryOne, body)).rejects.toThrowError(
+      'Invalid password reset token',
+    );
   });
 });
