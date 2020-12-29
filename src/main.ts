f775827c430bcb10aa55f36eef17c5ca061a249e
Tornike Goshadze <joffarex@gmail.com>
2020-12-30 00:51:11 +0400

    Setup server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'fastify-helmet';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+(async function () {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+
+  app.enableCors();
+  await app.register(helmet, {
+    contentSecurityPolicy: false,
+  });
+
+  const options = new DocumentBuilder()
+    .setTitle('Nodorum')
+    .setDescription('The Nodorum API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000, '0.0.0.0');
+})();
